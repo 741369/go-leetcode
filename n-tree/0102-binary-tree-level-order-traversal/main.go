541535f2de72b0914ecccf16ef5cb9290073fd51
lzw <1@lg1024.com>
2020-06-05 01:40:18 +0800

    add n-tree dfs and bfs

---
+++ n-tree/0102-binary-tree-level-order-traversal/main.go
@@ -0,0 +1,90 @@
+//给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+//
+//
+//
+// 示例：
+//二叉树：[3,9,20,null,null,15,7],
+//
+//    3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+//
+//
+// 返回其层次遍历结果：
+//
+// [
+//  [3],
+//  [9,20],
+//  [15,7]
+//]
+//
+// Related Topics 树 广度优先搜索
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+package main
+
+import "fmt"
+
+func main() {
+	tree := &TreeNode{
+		Val: 3,
+		Left: &TreeNode{
+			Val: 9,
+		},
+		Right: &TreeNode{
+			Val: 20,
+			Left: &TreeNode{
+				Val: 15,
+			},
+			Right: &TreeNode{
+				Val: 7,
+			},
+		},
+	}
+	res := levelOrder(tree)
+	fmt.Printf("%#v\n", res)
+
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func levelOrder(root *TreeNode) [][]int {
+	res := [][]int{}
+	if root == nil {
+		return res
+	}
+
+	q := []*TreeNode{root}
+	for i := 0; len(q) > 0; i++ {
+		res = append(res, []int{})
+		p := []*TreeNode{}
+		for j := 0; j < len(q); j++ {
+			node := q[j]
+			res[i] = append(res[i], node.Val)
+			if node.Left != nil {
+				p = append(p, node.Left)
+			}
+			if node.Right != nil {
+				p = append(p, node.Right)
+			}
+		}
+		q = p
+	}
+	return res
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
--- n-tree/0103-binary-tree-zigzag-level-order-traversal/main.go
+++ n-tree/0103-binary-tree-zigzag-level-order-traversal/main.go
@@ -1,9 +1,49 @@
+//给定一个二叉树，返回其节点值的锯齿形层次遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+//
+// 例如：
+//给定二叉树 [3,9,20,null,null,15,7],
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+//
+//
+// 返回锯齿形层次遍历如下：
+//
+// [
+//  [3],
+//  [20,9],
+//  [15,7]
+//]
+//
+// Related Topics 栈 树 广度优先搜索
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	tree := &TreeNode{
+		Val: 3,
+		Left: &TreeNode{
+			Val: 9,
+		},
+		Right: &TreeNode{
+			Val: 20,
+			Left: &TreeNode{
+				Val: 15,
+			},
+			Right: &TreeNode{
+				Val: 7,
+			},
+		},
+	}
+	res := zigzagLevelOrder(tree)
+	fmt.Printf("%#v\n", res)
 }
 
+//leetcode submit region begin(Prohibit modification and deletion)
 /**
  * Definition for a binary tree node.
  * type TreeNode struct {
@@ -12,6 +52,68 @@ func main() {
  *     Right *TreeNode
  * }
  */
- func zigzagLevelOrder(root *TreeNode) [][]int {
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
 
+// bfs搜索
+func zigzagLevelOrder2(root *TreeNode) [][]int {
+	res := [][]int{}
+	if root == nil {
+		return res
+	}
+	q := []*TreeNode{root}
+	flag := true
+	for len(q) > 0 {
+		qLen := len(q)
+		ans := make([]int, qLen)
+		for i := 0; i < qLen; i++ {
+			node := q[i]
+			if node.Left != nil {
+				q = append(q, node.Left)
+			}
+			if node.Right != nil {
+				q = append(q, node.Right)
+			}
+			if flag {
+				ans[i] = node.Val
+			} else {
+				ans[qLen-i-1] = node.Val
+			}
+		}
+		res = append(res, ans)
+		flag = !flag
+		q = q[qLen:]
+	}
+	return res
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+// dfs搜索
+
+func zigzagLevelOrder(root *TreeNode) [][]int {
+	res := [][]int{}
+	dfs(root, 0, &res)
+	return res
+}
+
+func dfs(root *TreeNode, level int, res *[][]int) {
+	if root == nil {
+		return
+	}
+	if level == len(*res) {
+		*res = append(*res, []int{})
+	}
+	// } else {
+	if level%2 == 0 {
+		(*res)[level] = append((*res)[level], root.Val)
+	} else {
+		(*res)[level] = append([]int{root.Val}, (*res)[level]...)
+	}
+	// }
+	dfs(root.Left, level+1, res)
+	dfs(root.Right, level+1, res)
 }
