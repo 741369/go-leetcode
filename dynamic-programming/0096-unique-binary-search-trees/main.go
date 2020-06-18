08bd87b0c2e7f6e72fa175f0492949479edd1c79
lzw <1@lg1024.com>
2020-06-18 23:50:19 +0800

    add n-tree and dynamic programming

---
+++ dynamic-programming/0096-unique-binary-search-trees/main.go
@@ -0,0 +1,47 @@
+/**********************************************
+** @Des:
+** @Author: lzw
+** @Last Modified time: 2020/6/18 下午11:27
+***********************************************/
+
+package main
+
+import "fmt"
+
+//给定一个整数 n，求以 1 ... n 为节点组成的二叉搜索树有多少种？
+//
+// 示例:
+//
+// 输入: 3
+//输出: 5
+//解释:
+//给定 n = 3, 一共有 5 种不同结构的二叉搜索树:
+//
+//   1         3     3      2      1
+//    \       /     /      / \      \
+//     3     2     1      1   3      2
+//    /     /       \                 \
+//   2     1         2                 3
+// Related Topics 树 动态规划
+
+func main() {
+	fmt.Println(numTrees(3))
+}
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func numTrees(n int) int {
+	// 动态规划
+	// G(n) = (i=1,n) G(i-1)*G(n-i) G(n)表示长度为n的二叉树的个数
+	arr := make([]int, n+1)
+	arr[0] = 1
+	arr[1] = 1
+	for i := 2; i <= n; i ++ {
+		for j := 1; j <= i ; j ++ {
+			arr[i] += arr[j-1]*arr[i-j]
+		}
+	}
+	return arr[n]
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
---
+++ n-tree/0098-validate-binary-search-tree/main.go
@@ -0,0 +1,107 @@
+/**********************************************
+** @Des:
+** @Author: lzw
+** @Last Modified time: 2020/6/18 下午10:34
+***********************************************/
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+//给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+//
+// 假设一个二叉搜索树具有如下特征：
+//
+//
+// 节点的左子树只包含小于当前节点的数。
+// 节点的右子树只包含大于当前节点的数。
+// 所有左子树和右子树自身必须也是二叉搜索树。
+//
+//
+// 示例 1:
+//
+// 输入:
+//    2
+//   / \
+//  1   3
+//输出: true
+//
+//
+// 示例 2:
+//
+// 输入:
+//    5
+//   / \
+//  1   4
+//     / \
+//    3   6
+//输出: false
+//解释: 输入为: [5,1,4,null,null,3,6]。
+//     根节点的值为 5 ，但是其右子节点值为 4 。
+//
+// Related Topics 树 深度优先搜索
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+type TreeNode struct {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+	root := &TreeNode{
+		Val:   2,
+		Left:  &TreeNode{Val: 1},
+		Right: &TreeNode{Val: 3},
+	}
+	fmt.Println(isValidBST(root))
+}
+
+func isValidBST(root *TreeNode) bool {
+	var stack []*TreeNode
+	min := math.MinInt64
+	for len(stack) > 0 || root != nil {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+		if min >= stack[len(stack)-1].Val {
+			return false
+		} else {
+			min = stack[len(stack)-1].Val
+		}
+		root = stack[len(stack)-1].Right
+		stack = stack[:len(stack)-1]
+	}
+	return true
+}
+
+
+func isValidBST2(root *TreeNode) bool {
+	return dfs(root, math.MinInt64, math.MaxInt64)
+}
+
+// 中序遍历
+func dfs(root *TreeNode, min int, max int) bool {
+	if root == nil {
+		return true
+	}
+	if root.Val <= min || root.Val >= max {
+		return false
+	}
+	return dfs(root.Left, min, root.Val) && dfs(root.Right, root.Val, max)
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
