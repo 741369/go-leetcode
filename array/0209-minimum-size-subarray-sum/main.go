782d27b996da828f978c4f7ed70b802c6aa1cf3f
lzw <1@lg1024.com>
2020-06-28 23:40:50 +0800

    add array

---
+++ array/0209-minimum-size-subarray-sum/main.go
@@ -0,0 +1,62 @@
+/**********************************************
+** @Des:
+** @Author: lzw
+** @Last Modified time: 2020/6/28 下午11:16
+***********************************************/
+
+package main
+
+import "fmt"
+
+//给定一个含有 n 个正整数的数组和一个正整数 s ，找出该数组中满足其和 ≥ s 的长度最小的连续子数组，并返回其长度。如果不存在符合条件的连续子数组，返回
+// 0。
+//
+//
+//
+// 示例：
+//
+// 输入：s = 7, nums = [2,3,1,2,4,3]
+//输出：2
+//解释：子数组 [4,3] 是该条件下的长度最小的连续子数组。
+//
+//
+//
+//
+// 进阶：
+//
+//
+// 如果你已经完成了 O(n) 时间复杂度的解法, 请尝试 O(n log n) 时间复杂度的解法。
+//
+// Related Topics 数组 双指针 二分查找
+func main() {
+	fmt.Println(minSubArrayLen(7, []int{2,3,1,2,4,3}))
+	fmt.Println(minSubArrayLen(4, []int{1,4,4}))
+}
+
+//leetcode submit region begin(Prohibit modification and deletion)
+// 暴力法
+func minSubArrayLen(s int, nums []int) int {
+	ans := len(nums) + 1
+	for i := 0; i < len(nums); i ++ {
+		tmp := 0
+		for j := i; j < len(nums); j ++ {
+			tmp += nums[j]
+			if tmp >= s {
+				ans = min(j-i+1, ans)
+				break
+			}
+		}
+	}
+	if ans == len(nums) + 1 {
+		return 0
+	}
+	return ans
+}
+func min(i, j int) int {
+	if i > j {
+		return j
+	}
+	return i
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
