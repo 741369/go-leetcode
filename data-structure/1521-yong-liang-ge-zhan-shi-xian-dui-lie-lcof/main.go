84fc914a626813284630318409cde08798762881
lzw <1@lg1024.com>
2020-07-01 00:55:55 +0800

    add queue and stack

---
+++ data-structure/1521-yong-liang-ge-zhan-shi-xian-dui-lie-lcof/main.go
@@ -0,0 +1,84 @@
+/**********************************************
+** @Des:
+** @Author: lzw
+** @Last Modified time: 2020/7/1 上午12:14
+***********************************************/
+
+package main
+
+import "container/list"
+
+//用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，分别完成在队列尾部插入整数和在队列头部删除整数的
+//功能。(若队列中没有元素，deleteHead 操作返回 -1 )
+//
+//
+//
+// 示例 1：
+//
+// 输入：
+//["CQueue","appendTail","deleteHead","deleteHead"]
+//[[],[3],[],[]]
+//输出：[null,null,3,-1]
+//
+//
+// 示例 2：
+//
+// 输入：
+//["CQueue","deleteHead","appendTail","appendTail","deleteHead","deleteHead"]
+//[[],[],[5],[2],[],[]]
+//输出：[null,-1,null,null,5,2]
+//
+//
+// 提示：
+//
+//
+// 1 <= values <= 10000
+// 最多会对 appendTail、deleteHead 进行 10000 次调用
+//
+// Related Topics 栈 设计
+
+// 栈先进后出，
+// 队列，先进先出
+
+//leetcode submit region begin(Prohibit modification and deletion)
+type CQueue struct {
+	stack1, stack2 *list.List
+}
+
+// 初始化2个栈
+// 第一个支持插入操作，第二个支持删除操作
+func Constructor() CQueue {
+	return CQueue{
+		stack1: list.New(),
+		stack2: list.New(),
+	}
+}
+
+// 入栈
+func (this *CQueue) AppendTail(value int)  {
+	this.stack1.PushBack(value)
+}
+
+// 出栈，并将出栈元素加入第二个栈，第二个栈出栈操作就是队列的出队操作
+func (this *CQueue) DeleteHead() int {
+	if this.stack2.Len() == 0 {
+		for this.stack1.Len() > 0 {
+			this.stack2.PushBack(this.stack1.Remove(this.stack1.Back()))
+		}
+	}
+	if this.stack2.Len() != 0 {
+		e := this.stack2.Back()
+		this.stack2.Remove(e)
+		return e.Value.(int)
+	}
+	return -1
+}
+
+
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.AppendTail(value);
+ * param_2 := obj.DeleteHead();
+ */
+//leetcode submit region end(Prohibit modification and deletion)
