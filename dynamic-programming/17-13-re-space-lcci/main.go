d4657b184aabd3dbf28f009c897fcad58e221c10
lzw <1@lg1024.com>
2020-07-09 13:26:01 +0800

    add dynamic programming

---
+++ dynamic-programming/17-13-re-space-lcci/main.go
@@ -0,0 +1,93 @@
+/**********************************************
+** @Des:
+** @Author: lzw
+** @Last Modified time: 2020/7/9 下午1:21
+***********************************************/
+
+package main
+
+//哦，不！你不小心把一个长篇文章中的空格、标点都删掉了，并且大写也弄成了小写。像句子"I reset the computer. It still didn’
+//t boot!"已经变成了"iresetthecomputeritstilldidntboot"。在处理标点符号和大小写之前，你得先把它断成词语。当然了，你有一
+//本厚厚的词典dictionary，不过，有些词没在词典里。假设文章用sentence表示，设计一个算法，把文章断开，要求未识别的字符最少，返回未识别的字符数。
+//
+//
+// 注意：本题相对原题稍作改动，只需返回未识别的字符数
+//
+//
+//
+// 示例：
+//
+// 输入：
+//dictionary = ["looked","just","like","her","brother"]
+//sentence = "jesslookedjustliketimherbrother"
+//输出： 7
+//解释： 断句后为"jess looked just like tim her brother"，共7个未识别字符。
+//
+//
+// 提示：
+//
+//
+// 0 <= len(sentence) <= 1000
+// dictionary中总字符数不超过 150000。
+// 你可以认为dictionary和sentence中只包含小写字母。
+//
+// Related Topics 记忆化 字符串
+func main() {
+	println(respace([]string{"looked","just","like","her","brother"}, "jesslookedjustliketimherbrother"))
+}
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func respace(dictionary []string, sentence string) int {
+	n, inf := len(sentence), 0x3f3f3f3f
+	root := &Trie{next: [26]*Trie{}}
+	for _, word := range dictionary {
+		root.insert(word)
+	}
+	dp := make([]int, n + 1)
+	for i := 1; i < len(dp); i++ {
+		dp[i] = inf
+	}
+	for i := 1; i <= n; i++ {
+		dp[i] = dp[i-1] + 1
+		curPos := root
+		for j := i; j >= 1; j-- {
+			t := int(sentence[j-1] - 'a')
+			if curPos.next[t] == nil {
+				break
+			} else if curPos.next[t].isEnd {
+				dp[i] = min(dp[i], dp[j-1])
+			}
+			if dp[i] == 0 {
+				break
+			}
+			curPos = curPos.next[t]
+		}
+	}
+	return dp[n]
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+type Trie struct {
+	next [26]*Trie
+	isEnd bool
+}
+
+func (this *Trie) insert(s string)  {
+	curPos := this
+	for i := len(s) - 1; i >= 0; i-- {
+		t := int(s[i] - 'a')
+		if curPos.next[t] == nil {
+			curPos.next[t] = &Trie{next: [26]*Trie{}}
+		}
+		curPos = curPos.next[t]
+	}
+	curPos.isEnd = true
+
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
