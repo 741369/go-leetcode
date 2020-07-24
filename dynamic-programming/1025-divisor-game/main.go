7a7252dc8a4f52b0de921cbbbbaed2a2cada87ae
lzw <1@lg1024.com>
2020-07-24 23:59:42 +0800

    change game

---
+++ dynamic-programming/1025-divisor-game/main.go
@@ -0,0 +1,11 @@
+/**********************************************
+** @Des:
+** @Author: lzw
+** @Last Modified time: 2020/7/24 下午11:59
+***********************************************/
+
+package main
+
+func divisorGame(N int) bool {
+	return N % 2 == 0
+}
