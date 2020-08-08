cae0e1218f0cde1c3f5fc9c4595f536f4a67e907
lzw <1@lg1024.com>
2020-08-08 22:35:30 +0800

    add n-tree

--- n-tree/0099-recover-binary-search-tree/main.go
+++ n-tree/0099-recover-binary-search-tree/main.go
@@ -6,6 +6,8 @@
 
 package main
 
+import "fmt"
+
 //二叉搜索树中的两个节点被错误地交换。
 //
 // 请在不改变其结构的情况下，恢复这棵树。
@@ -70,8 +72,63 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func recoverTree(root *TreeNode) {
+/**
+	1. 找到二叉搜索树中序遍历得到值序列的不满足条件的位置。
+	2. 如果有2个节点i，j满足，i < j && a[i] > a[i+1] && a[j] > a[j+1],则交换i, j+1即可
+	3. 如果只有1个节点i，满足，a[i] > a[i+1]，则交换i 和 i+1即可
+ */
+func recoverTree(root *TreeNode)  {
+	var nums []int
 
+	// 中序遍历二叉树
+	var inOrder func(node *TreeNode)
+	inOrder = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		inOrder(node.Left)
+		nums = append(nums, node.Val)
+		inOrder(node.Right)
+	}
+
+	inOrder(root)
+	x, y := findTwoSwapped(nums)
+	fmt.Println(x, y)
+	recover(root, 2, x, y)
+}
+
+func findTwoSwapped(nums []int) (int, int) {
+	x, y := -1, -1
+	for i := 0; i < len(nums) - 1; i++ {
+		if nums[i + 1] < nums[i] {
+			y = nums[i+1]
+			if x == -1 {
+				x = nums[i]
+			} else {
+				break
+			}
+		}
+	}
+	return x, y
+}
+
+func recover(root *TreeNode, count, x, y int) {
+	if root == nil {
+		return
+	}
+	if root.Val == x || root.Val == y {
+		if root.Val == x {
+			root.Val = y
+		} else {
+			root.Val = x
+		}
+		count--
+		if count == 0 {
+			return
+		}
+	}
+	recover(root.Right, count, x, y)
+	recover(root.Left, count, x, y)
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
