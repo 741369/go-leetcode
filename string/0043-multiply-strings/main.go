7147d36762f7ee785528187d7f32f3ffbe74e2b0
lzw <1@lg1024.com>
2020-08-13 21:59:43 +0800

    add string

--- string/0043-multiply-strings/main.go
+++ string/0043-multiply-strings/main.go
@@ -6,7 +6,10 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 //给定两个以字符串形式表示的非负整数 num1 和 num2，返回 num1 和 num2 的乘积，它们的乘积也表示为字符串形式。
 //
@@ -108,3 +111,31 @@ func addStrings(num1 string, num2 string) string {
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
+
+func multiply3(num1 string, num2 string) string {
+	if num1 == "0" || num2 == "0" {
+		return "0"
+	}
+	m, n := len(num1), len(num2)
+	ansArr := make([]int, m + n)
+	for i := m - 1; i >= 0; i-- {
+		x := int(num1[i]) - '0'
+		for j := n - 1; j >= 0; j-- {
+			y := int(num2[j] - '0')
+			ansArr[i + j + 1] += x * y
+		}
+	}
+	for i := m + n - 1; i > 0; i-- {
+		ansArr[i - 1] += ansArr[i] / 10
+		ansArr[i] %= 10
+	}
+	ans := ""
+	idx := 0
+	if ansArr[0] == 0 {
+		idx = 1
+	}
+	for ; idx < m + n; idx++ {
+		ans += strconv.Itoa(ansArr[idx])
+	}
+	return ans
+}
